f61ac2b4e24e7c62b8732e6c6c00afb6db0198cc
keengo <3189199@qq.com>
2022-12-28 12:39:10 +0800

    fix testdso add response content-type/cache-control

--- module/testdso/async_upstream.cpp
+++ module/testdso/async_upstream.cpp
@@ -5,7 +5,8 @@
 
 static void * create_ctx()
 {
-	return NULL;
+	test_context* t = new test_context(test_upstream);
+	return t;
 }
 static void free_ctx(void *ctx)
 {
@@ -17,11 +18,13 @@ static KGL_RESULT open(KREQUEST r, kgl_async_context *ctx)
 	test_context *t = (test_context *)ctx->module;
 	if (t->model == test_upstream) {
 		ctx->out->f->write_status(ctx->out, r, 200);
+		ctx->out->f->write_header(ctx->out, r, kgl_header_content_type, _KS("text/plain"));
+		ctx->out->f->write_header(ctx->out, r, kgl_header_cache_control, _KS("no-cache, no-store"));
 		KGL_RESULT result = ctx->out->f->write_header_finish(ctx->out, r);
 		if (result != KGL_OK) {
 			return result;
 		}
-		return ctx->out->f->write_end(ctx->out, r, ctx->out->f->write_body(ctx->out, r, kgl_expand_string("test_upstream_ok")));
+		return ctx->out->f->write_end(ctx->out, r, ctx->out->f->write_body(ctx->out, r, _KS("test_upstream_ok")));
 	}
 	KGL_RESULT result = ctx->f->open_next(r, ctx->cn, ctx->in, ctx->out, NULL);
 	ctx->in = NULL;
