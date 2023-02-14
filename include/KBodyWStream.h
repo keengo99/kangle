4edb9baaac8b2f62a130d61cc50a2a4595d6fc4d
keengo <3189199@qq.com>
2023-02-14 09:11:53 +0800

    add missing KBodyWStream.h

---
+++ include/KBodyWStream.h
@@ -0,0 +1,29 @@
+#ifndef KBODYWSTREAM_H_INCLUDED
+#define KBODYWSTREAM_H_INCLUDED
+#include "KStream.h"
+#include "ksapi.h"
+
+class KBodyWStream : public KWStream
+{
+public:
+	KBodyWStream() {
+		body = { 0 };
+	}
+	~KBodyWStream() {
+		assert(!body.ctx);
+		if (body.ctx) {
+			body.f->close(body.ctx, KGL_EUNKNOW);
+		}
+	}
+	KGL_RESULT write_end(KGL_RESULT result) override {
+		result = body.f->close(body.ctx, result);
+		body = { 0 };
+		return result;
+	}
+	kgl_response_body body;
+	KGL_RESULT write_all(const char* buf, int len) override {
+		return body.f->write(body.ctx, buf, len);
+	}
+};
+#endif
+
