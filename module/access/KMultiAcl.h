2603087374074c46d581429968ec937fbf8783c7
keengo99 <3189199@qq.com>
2024-08-26 19:57:42 +0800

    fix KMultiAcl parse text bug

--- module/access/KMultiAcl.h
+++ module/access/KMultiAcl.h
@@ -23,8 +23,8 @@
 #include "KXml.h"
 #include "utils.h"
 #include "krbtree.h"
-typedef int (* strcmpfunc)(const char *,const char *);
-class KMultiAcl: public KAcl {
+typedef int (*strcmpfunc)(const char*, const char*);
+class KMultiAcl : public KAcl {
 public:
 	KMultiAcl() {
 		seticase(true);
@@ -35,20 +35,20 @@ public:
 	virtual ~KMultiAcl() {
 		freeMap();
 	}
-	void get_html(KModel *model, KWStream &s) override {
+	void get_html(KModel* model, KWStream& s) override {
 		s << "<input name=v size=40 value='";
-		KMultiAcl *acl = (KMultiAcl *) (model);
+		KMultiAcl* acl = (KMultiAcl*)(model);
 		if (acl) {
 			acl->getValList(s);
 		}
 		s << "'>";
-		if(icase_can_change){
+		if (icase_can_change) {
 			s << "<input type=checkbox name=icase value='1' ";
 			if (acl == NULL || acl->icase) {
 				s << "checked";
 			}
 			s << ">ignore case,";
-		}	
+		}
 		s << "split:<input name=split max=1 size=1 value='";
 		if (acl == NULL) {
 			s << "|";
@@ -58,14 +58,14 @@ public:
 		s << "'>";
 	}
 	void getValList(KWStream& s) {
-		krb_node *node;
+		krb_node* node;
 		bool isFirst = true;
-		for (node=rb_first(&root); node!=NULL; node=rb_next(node)) {
+		for (node = rb_first(&root); node != NULL; node = rb_next(node)) {
 			if (!isFirst) {
 				s << split;
 			}
 			isFirst = false;
-			s << (char *)node->data;
+			s << (char*)node->data;
 		}
 		return;
 	}
@@ -77,7 +77,7 @@ public:
 	}
 	void parse_config(const khttpd::KXmlNodeBody* xml) override {
 		freeMap();
-		if(icase_can_change){
+		if (icase_can_change) {
 			if (xml->attributes["icase"] == "1") {
 				seticase(true);
 			} else {
@@ -89,94 +89,94 @@ public:
 		} else {
 			split = '|';
 		}
-		if (xml->attributes["v"].size() > 0) {
+		auto text = xml->get_text();
+		if (!text.empty()) {
+			explode(text.c_str());
+		}
+		if (!xml->attributes["v"].empty()) {
 			explode(xml->attributes["v"].c_str());
 		}
 	}
 protected:
-	bool match(const char *item) {
+	bool match(const char* item) {
 		if (item == NULL) {
 			return false;
 		}
-		struct krb_node **n = &(root.rb_node);
-		char *data;
+		struct krb_node** n = &(root.rb_node);
+		char* data;
 		while (*n) {
-			data = (char *)((*n)->data);
-			int result = cmp(item,data);
+			data = (char*)((*n)->data);
+			int result = cmp(item, data);
 			if (result < 0)
 				n = &((*n)->rb_left);
 			else if (result > 0)
 				n = &((*n)->rb_right);
 			else
-				return true;			
+				return true;
 		}
 		return false;
 	}
-	virtual char *transferItem(char *item)
-	{
+	virtual char* transferItem(char* item) {
 		return item;
 	}
 private:
-	void insert(char *item)
-	{
-		struct krb_node **n = &(root.rb_node), *parent = NULL;
-		char *data;
+	void insert(char* item) {
+		struct krb_node** n = &(root.rb_node), * parent = NULL;
+		char* data;
 		while (*n) {
-			data = (char *)((*n)->data);
-			int result = cmp(item,data);
+			data = (char*)((*n)->data);
+			int result = cmp(item, data);
 			parent = *n;
 			if (result < 0)
 				n = &((*n)->rb_left);
 			else if (result > 0)
 				n = &((*n)->rb_right);
-			else{
+			else {
 				free(item);
 				return;
 			}
 		}
-		krb_node *node = new krb_node;
+		krb_node* node = new krb_node;
 		node->data = item;
 		rb_link_node(node, parent, n);
 		rb_insert_color(node, &root);
 	}
-	void explode(const char *str)
-	{
-		char *buf = strdup(str);
-		char *hot = buf;
-		while(hot){
-			char *p = strchr(hot,split);
+	void explode(const char* str) {
+		char* buf = strdup(str);
+		char* hot = buf;
+		while (hot) {
+			char* p = strchr(hot, split);
 			if (p) {
 				*p = '\0';
 			}
-			if(*hot){
-				char *item = transferItem(strdup(hot));
+			if (*hot) {
+				char* item = transferItem(strdup(hot));
 				insert(item);
 			}
 			if (p) {
-				hot = p+1;
+				hot = p + 1;
 			} else {
 				hot = NULL;
-			}			
+			}
 		}
 		free(buf);
-	}	
+	}
 	void freeMap() {
-		for(;;){
-			krb_node *node = rb_first(&root);
-			if(node==NULL){
+		for (;;) {
+			krb_node* node = rb_first(&root);
+			if (node == NULL) {
 				break;
 			}
 			assert(node->data);
 			free(node->data);
-			rb_erase(node,&root);
+			rb_erase(node, &root);
 			delete node;
-		}		
+		}
 	}
 	strcmpfunc cmp;
 	char split;
 protected:
-	void seticase(bool icase)
-	{
+	void seticase(bool icase) {
 		this->icase = icase;
 		if (icase) {
 			cmp = strcasecmp;
