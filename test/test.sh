3036bd1ff4108a19da419ef79c61b1cc9aee54d0
keengo99 <3189199@qq.com>
2024-12-09 11:08:18 +0800

    cmake test

--- test/test.sh
+++ test/test.sh
@@ -1,10 +1,9 @@
 #!/bin/sh
-if [ ! -d ext ] ; then
-	mkdir ext
-fi
-cp -a ../webadmin .
-if [ ! -f ./bin/main.exe ] ; then
-	echo "please call ./build.sh first"
+BIN_FILE=$(readlink -f $0)
+PROJECT_PATH=$(dirname $BIN_FILE)
+cp -a $PROJECT_PATH/../webadmin $PROJECT_PATH
+if [ ! -f $PROJECT_PATH/bin/main.exe ] ; then
+	echo "please call $PROJECT_PATH/build.sh first"
 	exit 1
 fi
-./bin/main.exe -e ../build -m=true
+$PROJECT_PATH/bin/main.exe -e $PROJECT_PATH/../build -m=true
